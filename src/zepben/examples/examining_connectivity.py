2fe89f63f66edc4e93b843c08c0585b05605beb7
Marcus Koh <marcus.koh@zepben.com>
2022-11-22 18:27:18 +1100

    Add example of connected_terminals
    Signed-off-by: Marcus Koh <marcus.koh@zepben.com>

--- src/zepben/examples/examining_connectivity.py
+++ src/zepben/examples/examining_connectivity.py
@@ -4,7 +4,7 @@
 #  License, v. 2.0. If a copy of the MPL was not distributed with this
 #  file, You can obtain one at https://mozilla.org/MPL/2.0/.
 from zepben.evolve import EnergySource, AcLineSegment, Fuse, PowerTransformer, Breaker, EnergyConsumer, NetworkService, Terminal, connected_equipment, \
-    ConductingEquipment, PhaseCode
+    ConductingEquipment, PhaseCode, connected_terminals, ConnectivityResult
 
 # This example explores how to examine the immediate connectivity of equipment.
 # We will build a simple, linear network to examine:
@@ -57,22 +57,31 @@ network.connect_terminals(breaker_t2, lv_line_t1)
 network.connect_terminals(lv_line_t2, ec_t)
 
 
-def fancy_print_connectivity(equipment: ConductingEquipment, phases=None):
+def fancy_print_connectivity_result(connectivity_result: ConnectivityResult):
+    print(f"\t{connectivity_result.from_terminal} to {connectivity_result.to_terminal}")
+
+    terminal_str_len = len(str(connectivity_result.from_terminal))
+    for core_path in connectivity_result.nominal_phase_paths:
+        print(f"\t{core_path.from_phase.name:>{terminal_str_len}}----{core_path.to_phase.name}")
+
+
+def fancy_print_connected_equipment(equipment: ConductingEquipment, phases=None):
     if phases:
         print(f"Connectivity results for {equipment} on phases {phases}:")
     else:
         print(f"Connectivity results for {equipment}:")
     for connectivity_result in connected_equipment(equipment, phases):
-        print(f"\t{connectivity_result.from_terminal} to {connectivity_result.to_terminal}")
-
-        terminal_str_len = len(str(connectivity_result.from_terminal))
-        for core_path in connectivity_result.nominal_phase_paths:
-            print(f"\t{core_path.from_phase.name:>{terminal_str_len}}----{core_path.to_phase.name}")
+        fancy_print_connectivity_result(connectivity_result)
     print()
 
 
 # connected_equipment(equipment, phases) will get all connections between equipment cores matching one of the requested phases.
 # The connected equipment does not need to connect via all specified phases to appear in the list of connectivity results.
-fancy_print_connectivity(tx)
-fancy_print_connectivity(tx, phases=PhaseCode.N)
-fancy_print_connectivity(breaker, phases=PhaseCode.BC)
+fancy_print_connected_equipment(tx)
+fancy_print_connected_equipment(tx, phases=PhaseCode.N)
+fancy_print_connected_equipment(breaker, phases=PhaseCode.BC)
+
+# connected_terminals is essentially connected_equipment where only one terminal is considered.
+print(f"Connectivity results for terminal {lv_fuse_t2} on phases {PhaseCode.ACN}:")
+for connectivity_result in connected_terminals(lv_fuse_t2, PhaseCode.ACN):
+    fancy_print_connectivity_result(connectivity_result)
