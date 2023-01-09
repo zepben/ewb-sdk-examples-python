572b77e41aeb1650f0038d278d4824247bc5b82d
Marcus Koh <marcus.koh@zepben.com>
2023-01-09 12:37:18 +1100

    Use sdk version 0.35.0b7
    Signed-off-by: Marcus Koh <marcus.koh@zepben.com>

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     install_requires=[
         "zepben.auth==0.10.0b2",
         "zepben.eas==0.9.0b1",
-        "zepben.evolve==0.35.0b6",
+        "zepben.evolve==0.35.0b7",
         "zepben.protobuf==0.23.0b1",
         "zepben.edith==0.3.0b1",
         "pp-translator==0.7.0b1",
