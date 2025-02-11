45784caf8eed608ef52eee545787c2f586f9d331
Kurt Greaves <kurt.greaves@zepben.com>
2025-02-11 22:55:59 +1100

    Upgrade numba
    Signed-off-by: Kurt Greaves <kurt.greaves@zepben.com>

--- setup.py
+++ setup.py
@@ -11,10 +11,9 @@ setup(
     license="MPL 2.0",
     classifiers=[
          "Programming Language :: Python :: 3",
-         "Programming Language :: Python :: 3.7",
-         "Programming Language :: Python :: 3.8",
          "Programming Language :: Python :: 3.9",
          "Programming Language :: Python :: 3.10",
+         "Programming Language :: Python :: 3.11",
          "Operating System :: OS Independent"
      ],
     packages=find_namespace_packages(where="src"),
@@ -23,7 +22,7 @@ setup(
     install_requires=[
         "zepben.eas==0.16.0",
         "zepben.evolve==0.44.1",
-        "numba==0.56.4",
+        "numba==0.60.0",
         "geojson==2.5.0",
         "gql[requests]",
     ],
