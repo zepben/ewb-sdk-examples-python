48066658e62c4acc319fbdf154facdd52d610feb
Max Chesterfield <max.chesterfield@zepben.com>
2025-08-01 18:54:18 +1000

    needs a tidy up
    Signed-off-by: Max Chesterfield <max.chesterfield@zepben.com>

--- src/zepben/examples/emergy_consumer_device_hierarchy_downstream_context.py
+++ src/zepben/examples/emergy_consumer_device_hierarchy_downstream_context.py
@@ -6,18 +6,17 @@
 
 import asyncio
 import json
-import os
-import time
 import math
+import os
 from dataclasses import dataclass
+import time
 from typing import Dict
 
 import pandas as pd
-from zepben.ewb import NetworkConsumerClient, connect_with_token, Tracing, EnergyConsumer, PowerTransformer, \
-    TransformerFunctionKind, Breaker, Fuse, IdentifiedObject, EquipmentTreeBuilder, downstream, TreeNode, \
-    Feeder, IncludedEnergizedContainers
-import tqdm
 from tqdm.contrib.concurrent import process_map
+from zepben.evolve import connect_with_token, NetworkConsumerClient, Feeder, Tracing, downstream, StepActionWithContextValue, \
+    NetworkTraceStep, EnergyConsumer, StepContext, IdentifiedObject, Breaker, Fuse, PowerTransformer, TransformerFunctionKind
+from zepben.protobuf.nc.nc_requests_pb2 import IncludedEnergizedContainers
 
 
 @dataclass
@@ -48,22 +47,6 @@ async def get_feeders() -> Dict[str, Feeder]:
     return _feeders
 
 
-def process_leaf(up_data: dict, leaf: TreeNode):
-    to_equip: IdentifiedObject = leaf.identified_object
-
-    if isinstance(to_equip, Breaker):
-        if not up_data.get('breaker'):
-            up_data['breaker'] = to_equip
-    elif isinstance(to_equip, Fuse):
-        if not up_data.get('upstream_switch'):
-            up_data['upstream_switch'] = to_equip
-    elif isinstance(to_equip, PowerTransformer):
-        if not up_data.get('distribution_power_transformer'):
-            up_data['distribution_power_transformer'] = to_equip
-        elif not up_data.get('regulator') and to_equip.function == TransformerFunctionKind.voltageRegulator:
-            up_data['regulator'] = to_equip
-
-
 async def trace_from_feeder(feeder_mrid: str):
     """
     Fetch the equipment container from the given feeder and build an equipment tree of everything downstream of the feeder.
@@ -78,37 +61,45 @@ async def trace_from_feeder(feeder_mrid: str):
     )).throw_on_error()
     feeder = client.service.get(feeder_mrid, Feeder)
 
-    builder = EquipmentTreeBuilder()
+    energy_consumers = []
+
+    class StepActionWithContext(StepActionWithContextValue):
+        def _apply(self, item: NetworkTraceStep, context: StepContext):
+            if isinstance((ec := item.path.to_equipment), EnergyConsumer):
+                nonlocal energy_consumers
+                data = self.get_context_value(context)
+                data.update({'feeder': feeder.mrid, 'energy_consumer_mrid': ec.mrid})
+
+                row = _build_row(data)
+                energy_consumers.append(row)
+
+        def compute_next_value(self, next_item: NetworkTraceStep, current_item: NetworkTraceStep, current_value: Dict[str, IdentifiedObject]):
+            data = dict(current_value)
+            equip = next_item.path.to_equipment
+            if isinstance(equip, Breaker):
+                data['breaker'] = equip
+            elif isinstance(equip, Fuse):
+                data['upstream_switch'] = equip
+            elif isinstance(equip, PowerTransformer):
+                if equip.function == TransformerFunctionKind.distributionTransformer:
+                    data['distribution_power_transformer'] = equip
+                elif equip.function == TransformerFunctionKind.voltageRegulator:
+                    data['regulator'] = equip
+            return data
+
+        def compute_initial_value(self, item: NetworkTraceStep):
+            return {}
 
     await (
         Tracing.network_trace()
         .add_condition(downstream())
-        .add_step_action(builder)
+        .add_step_action(StepActionWithContext('key'))
     ).run(getattr(feeder, 'normal_head_terminal'))
-
-    energy_consumers = []
-
-    for leaf in (l for l in builder.leaves if isinstance((ec := l.identified_object), EnergyConsumer)):
-        ec_data = {'feeder': feeder.mrid, 'energy_consumer_mrid': ec.mrid}
-        def _process(_leaf):
-            process_leaf(ec_data, _leaf)
-            if _leaf.parent:
-                _process(_leaf.parent)
-        _process(leaf)
-
-        row = _build_row(ec_data)
-        energy_consumers.append(row)
 
     csv_sfx = "energy_consumers.csv"
     network_objects = pd.DataFrame(energy_consumers)
     os.makedirs("csvs", exist_ok=True)
     network_objects.to_csv(f"csvs/{feeder.mrid}_{csv_sfx}", index=False)
-
-
-class NullEquipment:
-    """empty class to simplify code below in the case of an equipment not existing in that position of the network"""
-    mrid = None
-    name = None
 
 
 def _build_row(up_data: dict[str, IdentifiedObject | str]) -> EnergyConsumerDeviceHierarchy:
@@ -125,6 +116,12 @@ def _build_row(up_data: dict[str, IdentifiedObject | str]) -> EnergyConsumerDevi
     )
 
 
+class NullEquipment:
+    """empty class to simplify code below in the case of an equipment not existing in that position of the network"""
+    mrid = None
+    name = None
+
+
 def main(_feeder):
     asyncio.run(trace_from_feeder(_feeder))
 
@@ -137,10 +134,10 @@ if __name__ == "__main__":
     print('processing feeders')
 
     # Process feeders sequentially.
-    for _feeder in tqdm.tqdm(feeders):
-        asyncio.run(trace_from_feeder(_feeder))
-    
+    # for _feeder in tqdm(feeders):
+    #     asyncio.run(trace_from_feeder(_feeder))
+
     # Process feeders concurrently.
-    #process_map(main, feeders, max_workers=math.floor(os.cpu_count()/2))
+    process_map(main, feeders, max_workers=math.floor(os.cpu_count()/2))
 
     print(f'done in {time.time() - start} seconds')
