3cb9e37ef0d079c213107d8591b97a52690f563a
Max Chesterfield <max.chesterfield@zepben.com>
2025-08-21 13:28:32 +1000

    Update project to use pyproject.toml (#31)
    Signed-off-by: Max Chesterfield <max.chesterfield@zepben.com>

--- setup.py
+++ setup.py
@@ -1,35 +1,9 @@
-from setuptools import setup, find_namespace_packages
+#  Copyright 2025 Zeppelin Bend Pty Ltd
+#
+#  This Source Code Form is subject to the terms of the Mozilla Public
+#  License, v. 2.0. If a copy of the MPL was not distributed with this
+#  file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-test_deps = ["pytest", "pytest-cov", "pytest-asyncio"]
-setup(
-    name="zepben.examples",
-    description="Module containing examples for interacting with Zepben's platform",
-    version="0.4.0b2",
-    url="https://github.com/zepben/ewb-sdk-examples-python",
-    author="Zeppelin Bend",
-    author_email="oss@zepben.com",
-    license="MPL 2.0",
-    classifiers=[
-         "Programming Language :: Python :: 3.10",
-         "Programming Language :: Python :: 3.11",
-         "Programming Language :: Python :: 3.12",
-         "Programming Language :: Python :: 3.13",
-         "Operating System :: OS Independent"
-     ],
-    packages=find_namespace_packages(where="src"),
-    package_dir={'': 'src'},
-    python_requires='>=3.9, <3.13',
-    install_requires=[
-        "zepben.eas==0.19.0",
-        "zepben.evolve==0.48.0",
-        "numba==0.60.0",
-        "geojson==2.5.0",
-        "gql[requests]==3.4.1",
-        "geopandas",
-        "pandas",
-        "shapely"
-    ],
-    extras_require={
-        "test": test_deps,
-    },
-)
+import setuptools
+
+setuptools.setup()
